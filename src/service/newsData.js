96d7a9ee27fd4543c4600ce300e043a3d1817bca
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 17:56:37 -0300

    added the filter language and the query to tesla

--- src/service/newsData.js
+++ src/service/newsData.js
@@ -1,7 +1,11 @@
 import api from "./api"
 
+/*/
+WITH THIS API IT IS POSSIBLE TO SEARCH AND CHOOSE THE LANGUAGE!
+*/
+
 const newsData = async () => {
-    return await api.get(`search?q=Tesla`).then(
+    return await api.get(`search?q="tecnologia"&lang=En`).then(
         (response) => {
             console.log(response)
             console.log(response.data.articles)
