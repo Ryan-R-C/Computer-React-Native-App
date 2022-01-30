9a5054b688bfefb5c2994fbb7b14821aa3ef2f25
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 17:14:47 -0300

    imported the newsData and presented it into the FlasList

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
 import { Text, SafeAreaView, FlatList } from 'react-native';
@@ -11,53 +11,19 @@ import Comment from '../../components/Comment'
 import Posts from '../Posts'
 
 import TextFormatedRegular from '../../components/TextFormated';
-import newsData from '../../service/loadNews';
-
-const appData = [
-    {
-        id: 1,
-        title: `The computers before screens`,
-        summary: `learn why there is so many "prints" in programming languages and how our lives are a little easy and cheap now...`,
-    },
-    {
-        id: 2,
-        title: `Why Machintosh was revolucionary`,
-        summary: `where "Windows" got it's name and why Steve Jobs where a genius...`,
-    },
-    {
-        id: 3,
-        title: `The revolution data`,
-        summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-    {
-      id: 4,
-      title: `The revolution data`,
-      summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-    {
-      id: 5,
-      title: `The revolution data`,
-      summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-    {
-      id: 6,
-      title: `The revolution data`,
-      summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-    {
-      id: 7,
-      title: `The revolution data`,
-      summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-    {
-      id: 8,
-      title: `The revolution data`,
-      summary: `Why so many jobs in nowadays are being extinct and so much more are being created...`,
-    },
-
-]
+import newsData from '../../service/newsData';
 
 export default function LandingPage() {
+  const [newsDatas, setNewsDatas] = useState([])
+
+  useEffect(
+    async () => {
+      const data = await newsData()
+      console.log("data")
+      console.log(data)
+      setNewsDatas(data)
+    }, []
+  )
     return (
       <SafeAreaView 
       style={
@@ -84,7 +50,7 @@ export default function LandingPage() {
         > */}
 
           <FlatList 
-          data={appData}
+          data={newsDatas}
           renderItem={
             ({item}) => (
               <Posts {...item} />
