91c920d5fe1ffd5a4fa9e9ec4c849dce4cf7c359
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 09:59:39 -0300

    refractored the code from App

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+import globalStyles from './src/globalStyles';
 
 import LandingPage from './src/screens/LandingPage'
 
 export default function App() {
   return (
-    <>
-      <LandingPage/>
-    </>
+    <SafeAreaView
+    style={
+      globalStyles.Globalbox
+    }
+    >
+      <LandingPage
+      />
+    </SafeAreaView>
   );
 }
