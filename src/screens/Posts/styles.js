e919496e6601d901b423f3385c7ae4c9d6704f23
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 17:16:47 -0300

    created the style to the new images

--- src/screens/Posts/styles.js
+++ src/screens/Posts/styles.js
@@ -11,6 +11,12 @@ const styles = StyleSheet.create({
     container:{
         backgroundColor: colors.secondBackgroundColor,
     },
+    image: {
+        width: '100%',
+        height: 200,
+        marginVertical: 10,
+        borderRadius: 10
+    }
 
 })
 
