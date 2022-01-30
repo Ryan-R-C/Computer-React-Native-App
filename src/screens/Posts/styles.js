7b179bbcbceed34f1346193c8687e9d45ade73b0
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 09:58:25 -0300

    added the basic style

---
+++ src/screens/Posts/styles.js
@@ -0,0 +1,18 @@
+
+
+import {StyleSheet, TouchableOpacity} from 'react-native';
+
+
+
+import { colors } from '../../globalStyles';
+
+
+export default styles = StyleSheet.create({
+    container:{
+        backgroundColor: colors.secondBackgroundColor,
+    },
+    text: {
+        color: colors.textColor
+    }
+
+})
