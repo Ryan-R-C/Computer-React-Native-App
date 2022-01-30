f13afdb678eb73e853bc2f653c3ae1c5f097309e
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-01-30 10:38:39 -0300

    imported poppins font

--- App.js
+++ App.js
@@ -5,7 +5,19 @@ import globalStyles from './src/globalStyles';
 
 import LandingPage from './src/screens/LandingPage'
 
+
+import { 
+  useFonts,
+  Poppins_400Regular,
+  Poppins_700Bold 
+} from '@expo-google-fonts/poppins'
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+      'PoppinsRegular': Poppins_400Regular, 
+      'PoppinsBold': Poppins_700Bold 
+    })
   return (
     <SafeAreaView
     style={
