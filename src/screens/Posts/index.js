e835985c6b5fc4b7008ac48e93016fc50377ecb9
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-12 15:57:16 -0300

    now the image open a new browser window and the structure of the news is cleaner

--- src/screens/Posts/index.js
+++ src/screens/Posts/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Linking, TouchableOpacity, View } from 'react-native';
 
 import { Image, Text, SafeAreaView, KeyboardAvoidingView } from 'react-native';
 
@@ -9,41 +10,58 @@ import TextFormatedRegular from '../../components/TextFormated';
 import styles from './styles';
 
 
-export default function Posts({ id, title, summary, media}) {
+export default function Posts({ _id, title, summary, media, link, authors}) {
     return (
       <SafeAreaView
+      onPress={() => Linking.canOpenURL('http://google.com')}
       style={
           globalStyles.postBox
       }
       >
-
-        <TextFormatedRegular
+      {/* <Text style={{color: 'blue'}}
+        onPress={() => Linking.openURL('http://google.com')}
+        >
+        Google
+      </Text> */}        
+      <TouchableOpacity
+      onPress={() => Linking.openURL(link)}
+      >
+        <Image source={{uri: media}}
+         style={styles.image}
+         />
+      </TouchableOpacity>
+     <View
+     style={
+      globalStyles.containerTextPostBox
+    }
+     >
+      <TextFormatedRegular
+        style={
+          globalStyles.title
+        }
+      >
+        { title }
+      </TextFormatedRegular>
+            {/*
+      <TextFormatedRegular
         style={
           globalStyles.text
-      }
-        >
-          { id }
-        </TextFormatedRegular>
+        }
+      >
+        { summary }
+      </TextFormatedRegular> */}
+      {/* <Comment /> */}
+     </View>
 
-        <TextFormatedRegular
-          style={
-            globalStyles.title
-          }
-        >
-          { title }
-        </TextFormatedRegular>
-        
-      <Image source={{uri: media}}
-       style={styles.image} />
-
-        <TextFormatedRegular
+      <TextFormatedRegular
           style={
             globalStyles.text
           }
         >
-          { summary }
+     { authors.map(
+            author => <TextFormatedRegular>{author}</TextFormatedRegular>
+          ) }
         </TextFormatedRegular>
-        {/* <Comment /> */}
 
 
       </SafeAreaView>
