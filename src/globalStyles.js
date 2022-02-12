7e22c4c3cd7689681d473434a281e8e1659e7008
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-12 15:58:08 -0300

    fixed the diplayed screen and created the search

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -35,12 +35,7 @@ const globalStyles = StyleSheet.create({
     },
     postBox: {
         backgroundColor: '#101828',
-        borderRadius: 3,
         display: 'flex',
-        paddingTop: 0.05 * windowWidth,
-        paddingLeft: 0.05 * windowWidth,
-        paddingBottom: 0.05 * windowWidth,
-        paddingRight: 0.05 * windowWidth,
         borderRadius: 10,
         marginHorizontal: 0.05 * windowWidth,
         marginVertical: 0.018 * windowHeight,
@@ -49,7 +44,14 @@ const globalStyles = StyleSheet.create({
         shadowOpacity: 0.2,
         shadowRadius: 3,
         maxWidth: 900,
+        // overflow: 'hidden'
         
+    },
+    containerTextPostBox:{
+        paddingTop: 0.05 * windowWidth,
+        paddingLeft: 0.05 * windowWidth,
+        paddingBottom: 0.05 * windowWidth,
+        paddingRight: 0.05 * windowWidth,
     },
 
     mainTitle: {
@@ -74,6 +76,51 @@ const globalStyles = StyleSheet.create({
         fontSize: 16,
         textAlign: 'justify',
         padding: 0.01 * windowHeight
+    },
+    search: {
+        display: 'flex',
+        height: 44,
+        backgroundColor: 'rgb(32, 33, 36)',
+        borderWidth: 1,
+        borderColor: '#5f6368',
+        borderTopColor: 'rgb(95, 99, 104)',
+        borderRightColor: 'rgb(95, 99, 104)',
+        borderBottomColor: 'rgb(95, 99, 104)',
+        borderLeftColor: 'rgb(95, 99, 104)',
+        borderRadius: 24,
+        marginVertical: 24,
+        width: 638,
+        width: 'auto',
+        maxWidth: 584,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    searchIcon:{
+        alignSelf: 'center',
+        margin: 0,
+        padding: 10,
+        color: '#e8eaed',
+        alignSelf: 'center',
+        display: 'flex',
+        flexDirection: 'row',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        // cursor: 'pointer'
+
+
+    },
+    searchInput: {  
+    margin: 0,
+    padding: 0,
+    color: '#e8eaed',
+    flex: 1,
+    height: 34,
+    fontSize: 16,   
+    borderColor: '#000000',
+    borderWidth: 0,
+    padding: 5,
+    height: '100%',
     }
 })
 
