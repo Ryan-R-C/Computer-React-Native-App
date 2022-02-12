b7040289d6c627a8c095fe466cc85e6125fd1378
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-12 16:13:21 -0300

    removed unsed code

--- src/service/newsSearch.js
+++ src/service/newsSearch.js
@@ -5,13 +5,8 @@ WITH THIS API IT IS POSSIBLE TO SEARCH AND CHOOSE THE LANGUAGE!
 */
 
 const newsSearch = async (lang, search) => {
-
-    console.log(lang)
-    // lang = 'PT'
     return await api.get(`search?q="${search}"&lang=${lang}`).then(
         (response) => {
-            // console.log(response)
-            // console.log(response.data.articles)
             return response.data.articles
         }
     )  
