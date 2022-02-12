d8378f736fd1a8399024af9ed386839d5acaed0b
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2022-02-12 15:58:35 -0300

    added the search input to landing page

--- src/screens/LandingPage/index.js
+++ src/screens/LandingPage/index.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { Text, SafeAreaView, FlatList } from 'react-native';
+import { Text, SafeAreaView, FlatList, Platform, TextInput, View, Keyboard } from 'react-native';
 
+import { AntDesign } from '@expo/vector-icons'; 
+
+
+//  AiOutlineSearch
+// import { FaSearch } from 'react-icons/fa';
 
 import globalStyles from '../../globalStyles';
 
@@ -11,17 +16,25 @@ import Comment from '../../components/Comment'
 import Posts from '../Posts'
 
 import TextFormatedRegular from '../../components/TextFormated';
-import newsData from '../../service/newsData';
+import newsData            from '../../service/newsData';
+import getLang             from '../../utils/getLang';
 
 export default function LandingPage() {
   const [newsDatas, setNewsDatas] = useState([])
+  const [lang, setLang] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const loadNews = async (lang) => {
+    const data = await newsData(lang)
+      setNewsDatas(data)
+      console.log(data)
+  }
 
   useEffect(
     async () => {
-      const data = await newsData()
-      console.log("data")
-      console.log(data)
-      setNewsDatas(data)
+      const userLanguage = getLang()
+      setLang(userLanguage)
+      loadNews(userLanguage)
     }, []
   )
     return (
@@ -30,16 +43,44 @@ export default function LandingPage() {
         globalStyles.subBox
       }
       >
-        {/* <StatusBar style="auto" /> */}
-        <TextFormatedRegular
-        style={
-          globalStyles.text,
-          globalStyles.mainTitle
-        }
-        >This is the landing page!</TextFormatedRegular>
-        <Text
-          style={globalStyles.text}
-        >Lets code!</Text>
+        <StatusBar style="none" />
+        
+      <View
+        style={globalStyles.search}
+      >
+        <View
+          style={globalStyles.searchIcon}
+          onClick={
+            (e) => console.log(searchQuery)
+          }
+        >
+
+        <AntDesign name="search1" size={24} color="white" />
+        
+        </View>
+        <TextInput 
+            keyboardType='default'
+            selectTextOnFocus
+            style={
+              // Platform.select({
+              //   web: {
+              //     outlineStyle: 'none',
+              //   }})
+              //   ,
+                globalStyles.searchInput
+            }
+
+            onChangeText={text => setSearchQuery(text)}
+            defaultValue={searchQuery}
+            onSubmitEditing={
+              () => {
+                console.log("enter hjahahahahahhahah")
+              }
+            }
+
+
+          />
+      </View>
 
 
         {/* <KeyboardAvoidingView
