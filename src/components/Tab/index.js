7ad2b6d4b3ed9692f27edaa73d0dd08286a4104b
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-02-20 11:17:21 -0300

    created the inital code to sidebar

---
+++ src/components/Tab/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { StatusBar } from 'expo-status-bar';
+import { TextInput } from 'react-native';
+
+import styles from './styles';
+
+
+export default function Comment() {
+    return (
+        <>
+        
+        
+        </>
+    )
+    }
